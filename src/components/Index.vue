af8cd9a7d29355b533073933dac0e236a6670533
peterfei <peterfeispace@gmail.com>
2016-12-27 15:54:58 +0800

    Fixed bugs

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -30,55 +30,57 @@
 	    }
 	}
 </script>
+
 <template>
-	<div class="container">
-		<div class="row">
-			<div class="product col-md-offset-2 col-sm-offset-2 col-md-8 col-sm-8">
-				<div class="row">
-					<div class="gallery col-md-6 col-sm-6">
-						<img  alt="" class="img-responsive">
-					</div>
-					<div class="detail col-md-6 col-sm-6">
-						<h3 class="name">
-							<span v-text="iPhone6S.name"></span>
-							
-						</h3>
-						<hr>
-						<div class="options">
-							<dl class="dl-horizontal">
-								<dt>描述：</dt>
-								<dd>
-									<span></span>
-								</dd>
-							</dl>
-							<dl class="price dl-horizontal">
-								<dt>价格：</dt>
-								<dd>
-									<span></span>
-								</dd>
-							</dl>
-							<dl class="option dl-horizontal">
-								<dt>外观</dt>
-								<dd>
-									
-								</dd>
-							</dl>
-							<dl class="option dl-horizontal">
-								<dt></dt>
-								<dd>
-									
-								</dd>
-							</dl>
-						</div>
-						 <hr>
-			            <button class="btn btn-danger btn-block" @click="addItem()" :disabled="iPhone6S.isSelected">
-			              <span class="glyphicon glyphicon-shopping-cart"></span> 加入购物车
-			            </button>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
+  <div class="container">
+    <div class="row">
+      <div class="product col-md-offset-2 col-sm-offset-2 col-md-8 col-sm-8">
+        <div class="row">
+          <div class="gallery col-md-6 col-sm-6">
+            <img :src="iPhone6S.activeStyleUrl" class="img-responsive" alt="">
+          </div>
+          <div class="detail col-md-6 col-sm-6">
+            <h3 class="name"><span v-text="iPhone6S.name"></span></h3>
+            <hr>
+            <div class="options">
+              <dl class="dl-horizontal">
+                <dt>描述：</dt>
+                <dd><span v-text="iPhone6S.desc"></span></dd>
+              </dl>
+              <dl class="price dl-horizontal">
+                <dt>价格：</dt>
+                <dd class="pomegranage"><strong>￥<span v-text="iPhone6S.price"></span></strong></dd>
+              </dl>
+              <dl class="option dl-horizontal">
+                <dt>外观：</dt>
+                <dd>
+                  <ul>
+                    <li v-for="(styleUrl,key) in iPhone6S.style"
+                        @click="changeStyle(key, styleUrl)"
+                        :class="{active: iPhone6S.activeStyleUrl == styleUrl}"><span v-text="key"></span></li>
+                  </ul>
+                </dd>
+              </dl>
+              <dl class="option dl-horizontal">
+                <dt>存储容量:</dt>
+                <dd>
+                  <ul>
+                    <li v-for="(price,key) in iPhone6S.storage"
+                        @click="changePrice(key, price)"
+                        :class="{active: iPhone6S.price == price}"><span v-text="key"></span></li>
+                  </ul>
+                </dd>
+              </dl>
+            </div>
+            <hr>
+            <button class="btn btn-danger btn-block" @click="addItem()" :disabled="iPhone6S.isSelected">
+              <span class="glyphicon glyphicon-shopping-cart"></span> 加入购物车
+            </button>
+          </div>
+        </div>
+      </div> <!-- ./product -->
+    </div>
+  </div><!-- ./container -->
 </template>
 
 <style>
